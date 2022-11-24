d8a19dffbd45f9db68084595f487cf1e48e09a7d
longvoct <volonglqd@gmail.com>
2022-11-24 15:21:30 +0700

    first commit

---
+++ BUS/DangNhapBUS.cs
@@ -0,0 +1,19 @@
+﻿using DAL;
+using DTO;
+using System.Collections.Generic;
+
+namespace BUS
+{
+    public class DangNhapBUS
+    {
+        ConnectionAccess obj_DAL = new ConnectionAccess();
+
+        public List<NhanVien> dangnhap(string taikhoan, string matkhau)
+        {
+            List<NhanVien> list = new List<NhanVien>();
+            list = obj_DAL.layDsNhanVien("select * from NhanVien5 where TKhoan = '" + taikhoan + "' and Mkhau = '" + matkhau + "' ");
+
+            return list;
+        }
+    }
+}
